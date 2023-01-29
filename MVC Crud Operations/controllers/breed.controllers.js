e64a0a7ac45c9646420066b6d6e41f84bb365ff0
AnupKamble <sahyadrihillsagro@gmail.com>
2023-01-29 20:52:54 +0530

    validator done

--- MVC Crud Operations/controllers/breed.controllers.js
+++ MVC Crud Operations/controllers/breed.controllers.js
@@ -1,69 +1,48 @@
-
-const express = require('express');
-const Breed = require('../models/breed.models')
-const {Router}=require('express');
-const fsPromises = require('fs/promises')
-const validator = require('../middlewares/validator')
-
+const express = require("express");
+const Breed = require("../models/breed.models");
+const { Router } = require("express");
+const fsPromises = require("fs/promises");
+const validator = require("../middlewares/validator");
+const records = require("../middlewares/records");
 
 const router = Router();
 
- 
-router.get("/", async (req,res)=> {
+router.get("/", async (req, res) => {
+  try {
+    const data = await Breed.find();
+    return res.status(200).send(data);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
 
-    try {
-        const data = await Breed.find();
-        return res.status(200).send(data)
-    }
-    catch(err) {
+router.post("/", validator, async (req, res) => {
+  try {
+    const data = await Breed.create(req.body);
+    return res.status(200).send(data);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
 
-        return res.status(500).send(err.message)
-    }
-})
+router.patch("/:id",records , async (req, res) => {
+  try {
+    const data = await Breed.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    return res.status(200).send(data);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
 
- 
-router.post("/", validator, async (req,res)=> {
-
-    try {
-          const data = await Breed.create(req.body)
-        return res.status(200).send(data)
-    }
-    catch(err) {
-
-        return res.status(500).send(err.message)
-    }
-})
-
- 
-router.patch("/:id", async (req,res)=> {
-
-    try { 
-          fsPromises.appendFile('records.txt',`The document with id:${req.params.id} has been updated\n`)
-          const data = await Breed.findByIdAndUpdate(req.params.id,req.body,{new:true})
-        return res.status(200).send(data)
-    }
-    catch(err) {
-
-        return res.status(500).send(err.message)
-    }
-})
-
- 
-router.delete("/:id", async (req,res)=> {
-
-    try {
-          fsPromises.appendFile('records.txt',`The document with id:${req.params.id} has been deleted\n`)
-          const data = await Breed.findByIdAndDelete(req.params.id)
-        return res.status(200).send(data)
-    }
-    catch(err) {
-
-        return res.status(500).send(err.message)
-    }
-})
-
-
-
-
+router.delete("/:id", records, async (req, res) => {
+  try {
+    const data = await Breed.findByIdAndDelete(req.params.id);
+    return res.status(200).send(data);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
 
 module.exports = router;
---
+++ MVC Crud Operations/middlewares/records.js
@@ -0,0 +1,16 @@
+const fsPromises = require('fs/promises')
+
+const records =( req , res , next )=> {
+  
+    if (req.method === 'DELETE' ) {
+
+        fsPromises.appendFile('records.txt',`The document with id:${req.params.id} has been Updated\n`)
+    }
+    else if ( req.method === 'PATCH' ) {
+        fsPromises.appendFile('records.txt',`The document with id:${req.params.id} has been Deleted\n`)
+    }
+
+    next();
+}
+
+module.exports = records;
